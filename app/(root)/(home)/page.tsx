eea982c8f548e508ca11c91f34a8003cf58ad1af
FurkanBey123 <furkanboran123@gmail.com>
2023-11-09 12:41:56 +0000

    feat: global layout add.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,6 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
+      <div className="h1-bold">Welcome To Dewflow</div>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
